96f8538cd3171c1b86ea53870aa9c04e610b2d00
{Anshika} <{anshika1008ya@gmail.com}>
2026-02-05 11:34:46 +0530

    Add new file

---
+++ platformlinkedlist/removeduplicate.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+
+        while (curr != NULL && curr->next != NULL) {
+            if (curr->val == curr->next->val) {
+                ListNode* temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp;
+            } else {
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp=head;
+        while(head!=NULL && head->next!=NULL){
+            if(head->val==head->next->val){
+                head->next=head->next->next;
+            }else{
+                head=head->next;
+            }
+        }
+        return temp;  
+    }
+};
