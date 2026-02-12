f56d7c5111ee5c1237509f63edb1726e5bcbeb72
{Anshika} <{anshika1008ya@gmail.com}>
2026-02-12 16:36:54 +0530

    Add new file

---
+++ trees/1.cpp
@@ -0,0 +1,21 @@
+/* 
+Tree-> It is a non linear,hierarchical data structure consisting of nodes connected by edges
+Binary Tree-> a tree is a binary tree when every node have not more than two children.
+*/
+
+
+#include<iostream>
+using namespace std;
+class Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+}
+int main(){
+
+}
