d113956e926d9ab6462eece41cbf5910e09618a0
{Anshika} <143735128+Anshika7820@users.noreply.github.com>
2026-02-26 12:38:45 +0530

    test commit

---
+++ graph/prerequissitetask.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+  
+  bool dfs(int node, vector<vector<int>> &adj,vector<int> &visited,vector<int> &rec){
+      visited[node]=1;
+      rec[node]=1;
+      
+      for(auto neigh: adj[node]){
+          if(!visited[neigh]){
+              if(dfs(neigh,adj,visited,rec)){
+                  return true;
+              }
+          }else if(rec[neigh]){
+              return true;
+          }
+      }
+      rec[node]=0;
+      return false;
+  }
+  
+    bool isPossible(int N, int P, vector<pair<int, int> >& pre) {
+       vector<vector<int>> adj(N);
+       for(auto &i: pre){
+           int u=i.first;
+           int v=i.second;
+       
+           adj[v].push_back(u);
+       }
+           vector<int> rec(N,0);
+           vector<int> visited(N,0);
+           for(int i=0;i<N;i++){
+               if(!visited[i]){
+                   if(dfs(i,adj,visited,rec)){
+                       return false;
+                   }
+               }
+           }
+           return true;
+    }
+};
